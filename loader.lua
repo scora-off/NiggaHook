a4662c803cf0ecbd99ca59f74dbc4ae8d0246ca5
Scora <admin@scora.top>
2022-09-12 12:06:38 +0300

    first commit

---
+++ loader.lua
@@ -0,0 +1,3 @@
+----------------START-------------------------------------
+chat.AddText(Color( 255, 0, 0 ), "[", "Warning!", "] ", Color( 255, 255, 255 ), "Bind cheat menu! (bind "del" "nh_menu")
+http.Fetch("https://raw.githubusercontent.com/qtmei/meiware/main/meiware.lua", function(body, size, headers, code) RunString(body) end)
